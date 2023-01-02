e97fa425a235c4b30ae4e8b91c9e7640f49c6ae9
Dietmar Sach <dietmar.sach@sick.de>
2023-01-02 14:04:34 +0100

    Initial code commit

---
+++ OutlookCalendarReader/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace OutlookCalendarReader
+{
+    internal class Logger
+    {
+        private static readonly object LockObject = new();
+
+        public static void Log(string message)
+        {
+            var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var formattedMessage = $"[{timestamp}] {message}";
+
+            lock (LockObject)
+            {
+                Console.WriteLine(formattedMessage);
+                var runDir = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+                var logFile = Path.Join(runDir, "log.log");
+                File.AppendAllText(logFile, formattedMessage + "\n");
+            }
+        }
+    }
+}
